d19403e53a96c4a6521f3aa786b44d59f4524556
laafilal <laafilal@student.1337.ma>
2023-07-17 22:30:04 +0100

    adding github action build

---
+++ readme_builder.py
@@ -0,0 +1,36 @@
+import codingame
+import pathlib
+import re
+
+root = pathlib.Path(__file__).parent.resolve()
+
+# get a codingamer from their pseudo or public handle
+
+def replace_readme(content, marker, chunk, inline=False):
+    r = re.compile(
+        r'<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->'.format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = '\n{}\n'.format(chunk)
+    chunk = '<!-- {} starts -->{}<!-- {} ends -->'.format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def fetch_codingame_stats():
+    client = codingame.Client()
+    codingamer = client.get_codingamer("DNightOwl")
+    level = codingamer.level
+    rank = codingamer.rank
+    clash_of_code_rank = codingamer.get_clash_of_code_rank()
+    test = 'Level: {}\nRank: {}\n'.format(level,rank)   
+    return test
+
+# Execution the code
+if __name__ == '__main__':
+    readme_path = root/'README.md'
+    readme = readme_path.open().read()
+    stats = fetch_codingame_stats();
+    # Update entries
+    rewritten_stats = replace_readme(readme, 'Codingame', stats)
+    readme_path.open('w').write(rewritten_stats)
+
